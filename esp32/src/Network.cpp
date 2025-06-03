86ea1ab136a981d31ebadb2fd37791fe225226ae
lucasmedeirosleite <lucastoc@gmail.com>
2025-06-03 17:31:30 -0400

    Refatorando comunicação com server

--- esp32/src/Network.cpp
+++ esp32/src/Network.cpp
@@ -20,46 +20,52 @@ int sendPrecipitationData(const String &jsonPayload) {
   Serial.print("HTTP status: ");
   Serial.println(httpCode);
 
-  if (httpCode > 0) {
-    String res = http.getString();
-    Serial.print("Response: ");
-    Serial.println(res);
-    
-    if (res.length() > 0) {
-      JsonDocument doc;
-      auto err = deserializeJson(doc, res);
-      
-      if (!err) {
-        bool predictResultIsBoolean = doc["enchente"].is<bool>();
-
-        if (predictResultIsBoolean) {
-          bool predictionResult = doc["enchente"].as<bool>();
-
-          if (predictionResult) {
-            result = SERVER_RESPONSE_FLOOD;
-          } else {
-            result = SERVER_RESPONSE_NO_FLOOD;
-          }
-        } else {
-          result = SERVER_RESPONSE_ERROR;
-        }
-      } else {
-        Serial.print("JSON parsing error: ");
-        Serial.println(err.c_str());
-        
-        result = SERVER_RESPONSE_ERROR;
-      }
-    } else {
-      Serial.println("Empty response received");
-      result = SERVER_RESPONSE_ERROR;
-    }
-  } else {
+  if (httpCode <= 0) {
     Serial.print("HTTP error: ");
     Serial.println(http.errorToString(httpCode).c_str());
     
-    result = SERVER_RESPONSE_ERROR;
+    http.end();
+    return SERVER_RESPONSE_ERROR;
   }
+
+  String res = http.getString();
+  Serial.print("Response: ");
+  Serial.println(res);
+
+  if (res.length() <= 0) {
+    Serial.println("Empty response received");
+    
+    http.end();
+    return SERVER_RESPONSE_ERROR;
+  }
+
+  JsonDocument doc;
+  auto err = deserializeJson(doc, res);
+
+  if (err) {
+    Serial.println("JSON parsing error");
+
+    http.end();
+    return SERVER_RESPONSE_ERROR;
+  }
+
+  bool predictResultIsBoolean = doc["enchente"].is<bool>();
+
+  if (!predictResultIsBoolean) {
+    Serial.println("Unexpected JSON structure");
+
+    http.end();
+    return SERVER_RESPONSE_ERROR;
+  }
+
+  bool predictionResult = doc["enchente"].as<bool>();
   
+  if (predictionResult) {
+    result = SERVER_RESPONSE_FLOOD;
+  } else {
+    result = SERVER_RESPONSE_NO_FLOOD;
+  }
+
   http.end();
   return result;
 }
